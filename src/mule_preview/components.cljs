1d44825a890d190c8ff7e1d3df752898c5354966
Sean Dawson <contact@seandawson.info>
2018-09-28 15:31:15 +1000

    Normalised split names

--- src/mule_preview/components.cljs
+++ src/mule_preview/components.cljs
@@ -1,10 +1,19 @@
 (ns mule-preview.components
   (:require 
     [reagent.core :as r]
+    [clojure.string :refer [split]]
     [mule-preview.mappings :refer [element-to-icon-map]]))
 
+(defn- normalise-name [name]
+  (let [split-name (split name #":")
+        first-component (first split-name)]
+    (if (> (count split-name) 1)
+       (str first-component ":*")
+       first-component)))
+
 (defn- name-to-img-url [name]
- (str "img/icons/" (get element-to-icon-map name "generic-component-48x32.png")))
+  (let [normalised-name (normalise-name name)]
+    (str "img/icons/" (get element-to-icon-map normalised-name "generic-component-48x32.png"))))
 
 (defn mule-component [name description]
   [:div {:class "component"}
