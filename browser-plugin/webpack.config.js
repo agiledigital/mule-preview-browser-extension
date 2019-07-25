098ab1feb0b13c2fb7bb3b0656bdeaf0bdabd2ee
Sean Dawson <spdawson@agiledigital.com.au>
2019-07-25 18:16:48 +1000

    Split out shared webpack config

--- browser-plugin/webpack.config.js
+++ browser-plugin/webpack.config.js
@@ -1,63 +1,12 @@
 const webpack = require("webpack");
-const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const StyleLintPlugin = require("stylelint-webpack-plugin");
+const sharedConfig = require("./webpack.shared.config");
 
 module.exports = {
+  ...sharedConfig,
+
   mode: "development",
-
-  entry: {
-    main: path.resolve(__dirname, "src/app/main.js"),
-    background: path.resolve(__dirname, "src/app/background.js")
-  },
-
-  output: {
-    path: path.resolve(__dirname, "extension/dist"),
-    filename: "[name].js"
-  },
-
-  resolve: {
-    extensions: [".js", ".json", ".scss", ".css"]
-  },
-
-  module: {
-    rules: [
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "eslint-loader",
-        exclude: [/node_modules/, /release.js/],
-        options: {
-          cache: true
-        }
-      },
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-        exclude: [/node_modules/, /release.js/]
-      },
-      {
-        test: /\.html$/,
-        loaders: ["html-loader"]
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          "css-loader",
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        loader: "url-loader",
-        options: {
-          limit: 10000
-        }
-      }
-    ]
-  },
 
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
@@ -67,7 +16,8 @@ module.exports = {
       // all options are optional
       filename: "[name].css",
       chunkFilename: "[id].css"
-    })
+    }),
+    new StyleLintPlugin()
   ],
 
   devtool: "eval-cheap-module-source-map"
--- browser-plugin/webpack.production.config.js
+++ browser-plugin/webpack.production.config.js
@@ -1,65 +1,14 @@
-const webpack = require("webpack");
-const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const StyleLintPlugin = require("stylelint-webpack-plugin");
+const sharedConfig = require("./webpack.shared.config");
 
 module.exports = {
+  ...sharedConfig,
+
   mode: "production",
 
-  entry: {
-    main: path.resolve(__dirname, "src/app/main.js"),
-    background: path.resolve(__dirname, "src/app/background.js")
-  },
-
-  output: {
-    path: path.resolve(__dirname, "extension/dist"),
-    filename: "[name].js"
-  },
-
-  resolve: {
-    extensions: [".js", ".json", ".scss", ".css"]
-  },
-
-  module: {
-    rules: [
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "eslint-loader",
-        exclude: [/node_modules/, /release.js/],
-        options: {
-          cache: true
-        }
-      },
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-        exclude: [/node_modules/, /release.js/]
-      },
-      {
-        test: /\.html$/,
-        loaders: ["html-loader"]
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          "css-loader",
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        loader: "url-loader",
-        options: {
-          limit: 10000
-        }
-      }
-    ]
-  },
-
   plugins: [
+    new StyleLintPlugin(),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // all options are optional
@@ -68,5 +17,5 @@ module.exports = {
     })
   ],
 
-  devtool: "cheap-module-source-map"
+  devtool: "source-map"
 };
