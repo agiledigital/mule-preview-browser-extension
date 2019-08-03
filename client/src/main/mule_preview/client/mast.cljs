ad20800724d2b8656aa399763d484bd9e61d2621
Sean Dawson <spdawson@agiledigital.com.au>
2019-08-03 22:02:25 +1000

    More style fixes for MUnit

--- client/src/main/mule_preview/client/mast.cljs
+++ client/src/main/mule_preview/client/mast.cljs
@@ -3,7 +3,7 @@
   (:require
    [clojure.walk :refer [prewalk]]
    [clojure.set :refer [union]]
-   [clojure.string :refer [starts-with?]]
+   [clojure.string :refer [starts-with? split replace capitalize]]
    [mule-preview.client.utils :refer [remove-location]]
    [mule-preview.client.mappings :refer [root-container horizontal-container-list
                                          vertical-container-list error-handler-component-list
@@ -11,6 +11,11 @@
 
 (defn- get-tag [node]
   (name (node :tag)))
+
+(defn- get-munit-title [tag-name]
+  (let [munit-flow-name (last (split tag-name #":"))
+        spaced (replace munit-flow-name #"-" " ")]
+    (capitalize spaced)))
 
 (defn- is-error-handler [node]
   (let [tag (get-tag node)
@@ -83,12 +88,14 @@
   (let [description (get-description node)
         content (node :content)
         {mocks true regular-components false}
-        (group-by is-munit-mock-component content)]
+        (group-by is-munit-mock-component content)
+        title (get-munit-title tag-name)
+        is-split-flow (= tag-name "test")]
     {:type :munit-container
      :tag-name tag-name
      :description description
-     :content [(create-mule-psuedo-container mocks #{:top} "Setup")
-               (create-mule-psuedo-container regular-components #{:bottom} "Test")]
+     :content [(create-mule-psuedo-container mocks #{:top} (if is-split-flow "Setup" title))
+               (create-mule-psuedo-container regular-components #{:bottom} (if is-split-flow "Test" ""))]
      :labels labels
      :location (:location node)}))
 
@@ -102,7 +109,7 @@
         is-vertical-container (contains? vertical-container-list tag-name)]
     (cond
       is-root-container (create-mule-container-component node tag-name #{:root :vertical})
-      is-munit-container (process-munit-container node tag-name #{:vertical})
+      is-munit-container (process-munit-container node tag-name #{:vertical :munit})
       is-error-handler-container (process-error-container node tag-name #{:vertical})
       is-error-handler-component (create-mule-container-component node tag-name #{:error-handler})
       is-horizontal-container (create-mule-container-component node tag-name #{:horizontal})
